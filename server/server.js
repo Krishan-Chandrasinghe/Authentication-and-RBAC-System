24c4c583ea8a05d7eaead3eb00a2fd4674269fcf
Krishan-Malinda <krishanmalinda99@gmail.com>
2025-10-07 22:52:10 +0530

    URL fixed

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: `${process.env.FRONTEND_URL}`, credentials: true }));
+app.use(cors({ origin: 'https://rbac-system-mern-front.vercel.app', credentials: true }));
 app.use((req,res,next) => {
   console.log(req.path, req.method);
   next();
